680ca2bb7234340ab98b17f7df0fa93104cdf5ad
DylanLale <disneygamer706@gmail.com>
2022-11-21 08:56:39 -0500

    DONE

--- Game1.cs
+++ Game1.cs
@@ -15,7 +15,12 @@ namespace Part_3__Animation
         Texture2D tribblegreyTexture;
         Rectangle tribblegreyRect;
         Vector2 tribblegreySpeed;
-
+        Rectangle tribblebrownRect;
+        Vector2 tribblebrownSpeed;
+        Rectangle tribblecreamRect;
+        Vector2 tribblecreamSpeed;
+        Rectangle tribbleorangeRect;
+        Vector2 tribbleorangeSpeed;
 
         public Game1()
         {
@@ -29,7 +34,14 @@ namespace Part_3__Animation
             // TODO: Add your initialization logic here
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 500;
+            tribblegreySpeed = new Vector2(2, 0);
             tribblegreyRect = new Rectangle(300, 10, 100, 100);
+            tribblebrownSpeed = new Vector2(1, 2);
+            tribblebrownRect = new Rectangle(300, 10, 100, 100);
+            tribblecreamSpeed = new Vector2(3, 4);
+            tribblecreamRect = new Rectangle(200, 10, 100, 100);
+            tribbleorangeSpeed = new Vector2(0, 2);
+            tribbleorangeRect = new Rectangle(400, 30, 100, 100);
             base.Initialize();
         }
 
@@ -51,6 +63,29 @@ namespace Part_3__Animation
 
         protected override void Update(GameTime gameTime)
         {
+            tribblegreyRect.X += (int)tribblegreySpeed.X;
+            tribblegreyRect.Y += (int)tribblegreySpeed.Y;
+            if (tribblegreyRect.Right > 800 || tribblegreyRect.Left < 0)
+                tribblegreySpeed.X *= -1;
+
+            tribblebrownRect.X += (int)tribblebrownSpeed.X;
+            tribblebrownRect.Y += (int)tribblebrownSpeed.Y;
+            if (tribblebrownRect.Right > 800 || tribblebrownRect.Left < 0)
+                tribblebrownSpeed.X *= -1;
+            if (tribblebrownRect.Bottom > _graphics.PreferredBackBufferHeight || tribblebrownRect.Top < 0)
+                tribblebrownSpeed.Y *= -1;
+
+            tribblecreamRect.X += (int)tribblecreamSpeed.X;
+            tribblecreamRect.Y += (int)tribblecreamSpeed.Y;
+            if (tribblecreamRect.Right > 800 || tribblecreamRect.Left > 0)
+                tribblecreamSpeed.X *= -1;
+            if (tribblecreamRect.Bottom > _graphics.PreferredBackBufferHeight || tribblecreamRect.Top < 0)
+                tribblecreamSpeed.Y *= -1;
+
+            tribbleorangeRect.X += (int)tribbleorangeSpeed.X;
+            tribbleorangeRect.Y += (int)tribbleorangeSpeed.Y;
+            if (tribbleorangeRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleorangeRect.Top < 0)
+                tribbleorangeSpeed.Y *= -1;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -65,6 +100,9 @@ namespace Part_3__Animation
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(tribblegreyTexture, tribblegreyRect, Color.White);
+            _spriteBatch.Draw(tribblecreamTexture, tribblecreamRect, Color.White);
+            _spriteBatch.Draw(tribbleorangeTexture, tribbleorangeRect, Color.White);
+            _spriteBatch.Draw(tribblebrownTexture, tribblebrownRect, Color.White);
 
             _spriteBatch.End();
             // TODO: Add your drawing code here
